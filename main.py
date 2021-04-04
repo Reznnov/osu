0418c1975e370c470889c10eca8c7f8f29bd1eb8
trettier <71816943+trettier@users.noreply.github.com>
2021-04-04 18:43:59 +0300

    meh
    meh

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import os
 
 
 def load_image(name, colorkey=None, convert=None):
-    fullname = os.path.join("data", name)
+    fullname = os.path.join(name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = sdl2.ext.load_image(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    if convert is not None:
-        image = pygame.transform.scale(image, convert)
+    # if colorkey is not None:
+    #     image = image.convert()
+    #     if colorkey == -1:
+    #         colorkey = image.get_at((0, 0))
+    #     image.set_colorkey(colorkey)
+    # else:
+    #     image = image.convert_alpha()
+    # if convert is not None:
+    #     image = pygame.transform.scale(image, convert)
     return image
 
 
@@ -36,12 +36,16 @@ class song():
     pass
 
 
-class note(sdl2.ext.Entity):
-    image1 = sdl2.ext.load_image("default-1.png")
-
-    def __init__(self, world, sprite, posx=0, posy=0):
+class note_sprite(sdl2.ext.Entity):
+    def __init__(self, world, sprite, posx=100, posy=100):
         self.sprite = sprite
         self.sprite.position = posx, posy
+
+
+class Note(sdl2.ext.Applicator):
+    def __init__(self):
+        super().__init__()
+        self.componenttypes = note_sprite, sdl2.ext.Sprite
 
     def start_timer(self):
         self.status = True
@@ -56,38 +60,49 @@ class note(sdl2.ext.Entity):
         return sdl2.timer.SDL_GetTicks() - self.startTicks
 
 
+    def process(self, world, componentsets):
+        for note_sprite, sprite in componentsets:
+            sprite.x += 50
+
+
+
 def run():
     sdl2.ext.init()
 
     window = sdl2.ext.Window("The Pong Game", size=(800, 600))
     world = sdl2.ext.World()
 
+
+    note = Note()
     spriterenderer = SoftwareRenderer(window)
     world.add_system(spriterenderer)
+    world.add_system(note)
+
 
     factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-
+    sp_ball = load_image("screenshot023.png")
+    note_sp = note_sprite(world, sp_ball)
     window.show()
     running = True
     state = sdl2.mouse.SDL_GetMouseState(None, None)
-    a = note()
+
 
     while running:
         events = sdl2.ext.get_events()
 
         for event in events:
             if event.key.keysym.sym == sdl2.SDLK_z:
-                a.start_timer()
+                note.start_timer()
                 print("n")
             if event.type == sdl2.SDL_MOUSEBUTTONDOWN:
                 motion = event.motion
                 print(motion.x, motion.y)
                 print(sdl2.timer.SDL_GetTicks() / 1000)
-                print(a.get_ticks() / 1000)
+                print(note.get_ticks() / 1000)
             if event.type == sdl2.SDL_QUIT:
                 running = False
                 break
-        window.process()
+        world.process()
     return 0
 
 
