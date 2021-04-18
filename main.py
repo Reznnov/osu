dc1002b91af1e3449884e0697d65595985ead8bd
trettier <71816943+trettier@users.noreply.github.com>
2021-04-18 13:22:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -44,19 +44,42 @@ class note_sprite(sdl2.ext.Entity):
         self.sprite.position = posx, posy
 
 
-class Velocity(object):
-    def __init__(self):
-        super(Velocity, self).__init__()
-        self.vx = 0
-        self.vy = 0
+class game_pr():
+    def __init__(self, world):
+        # for i in range(10):
+        #     n.append(Note())
+        note = Note()
+        world.add_system(note)
+        sdl2.ext.init()
+        factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+        sp_ball = factory.from_image("screenshot023.png")
+        note_sp = note_sprite(world, sp_ball)
+        note.note = note_sp
+        running = True
+        while running:
+            events = sdl2.ext.get_events()
+
+            for event in events:
+                if event.key.keysym.sym == sdl2.SDLK_z:
+                    note.start_timer()
+                    print("n")
+                if event.type == sdl2.SDL_MOUSEBUTTONDOWN:
+                    motion = event.motion
+                    print(motion.x, motion.y)
+                    print(sdl2.timer.SDL_GetTicks() / 1000)
+                    print(note.get_ticks() / 1000)
+                if event.type == sdl2.SDL_QUIT:
+                    running = False
+                    break
+            world.process()
+
 
 
 class Note(sdl2.ext.Applicator):
     def __init__(self):
         super().__init__()
-        print(self.componenttypes)
         self.componenttypes = note_sprite, sdl2.ext.Sprite
-        print(self.componenttypes)
+        self.note = None
 
     def start_timer(self):
         self.status = True
@@ -70,48 +93,45 @@ class Note(sdl2.ext.Applicator):
     def get_ticks(self):
         return sdl2.timer.SDL_GetTicks() - self.startTicks
 
+    def _overlap(self, item):
+        pos, sprite = item
+        left, top, right, bottom = sprite.area
+        return (right,  left, top, bottom)
+
     def process(self, world, componentsets):
         collitems = [comp for comp in componentsets if self._overlap(comp)]
-        print(collitems)
+        print(self.note.sprite.x)
         for sprite in componentsets:
             print(sprite)
             print("smh")
-            sprite.x += 50
+            self.note.sprite.x += 50
 
 
 def run():
-    sdl2.ext.init()
-
-    window = sdl2.ext.Window("The Pong Game", size=(800, 600))
+    window = sdl2.ext.Window("The Pong Game", size=(1600, 900))
     world = sdl2.ext.World()
+    # f = map(open("map.txt").read().split(), int)
 
 
-    note = Note()
     spriterenderer = SoftwareRenderer(window)
     world.add_system(spriterenderer)
-    world.add_system(note)
+
+    # world.delete_entities()
 
 
-    factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-    sp_ball = factory.from_image("screenshot023.png")
-    note_sp = note_sprite(world, sp_ball)
+
     window.show()
     running = True
     state = sdl2.mouse.SDL_GetMouseState(None, None)
 
+    # print(note.note.sprite.x)
 
     while running:
         events = sdl2.ext.get_events()
 
         for event in events:
-            if event.key.keysym.sym == sdl2.SDLK_z:
-                note.start_timer()
-                print("n")
-            if event.type == sdl2.SDL_MOUSEBUTTONDOWN:
-                motion = event.motion
-                print(motion.x, motion.y)
-                print(sdl2.timer.SDL_GetTicks() / 1000)
-                print(note.get_ticks() / 1000)
+            if event.key.keysym.sym == sdl2.SDLK_q:
+                game_pr(world)
             if event.type == sdl2.SDL_QUIT:
                 running = False
                 break
